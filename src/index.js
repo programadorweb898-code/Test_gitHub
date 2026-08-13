8c62d9446a22625785c6a5feb7159c1645b0373f
* <programadorweb@gmail.com>
2026-08-12 21:55:57 -0300

    archivo modificado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app=express();
 app.get("/",(req,res)=>{
-  res.send("test exitoso en esta respuesta");
+  res.send("test exitoso");
 });
 
 app.get("/test",(req,res)=>{
